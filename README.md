5a496fe2cfedab6954fcb604db59abb54d7421d9
Uthan H <uthan.hercules12@gmail.com>
2024-05-25 17:17:11 -0300

    Beta build

---
+++ README.md
@@ -0,0 +1,75 @@
+[QUEST BASIC]
+Trata-se de uma linguagem baseada em BASIC para facilitar a criação de jogos de aventura textual. Com ela, é possível executar jogos programados no navegador web utilizando a Quest Engine. Vamos a iniciar:
+
+[A SINTAXE]
+Por se basear em BASIC, utilizaremos pointers numéricos para representar os pontos em história. Não é mais necessário escrever os pointers como múltiplos de 10. Isso é feito apenas como uma boa prática. A sintaxe é a seguinte:
+
+|POINTER| |COMANDO| |DECLARAÇÃO|
+
+Essa é a forma geral de como todos os comandos serão executados. Vamos lista-los abaixo.
+
+[OPERADORES LÓGICOS]
+
+- Igual: =
+- Diferente: <>
+- Maior: >
+- Menor: <
+- Maior ou igual: >=
+- Menor ou igual: <=
+
+[SET]
+Define uma variável.
+
+Exemplo:
+0 SET NOME$ = "Maria"
+
+[PRINT]
+Renderiza um texto em tela. É possível imprimir variáveis ao colocá-las entre colchetes.
+
+Exemplo:
+0 PRINT "Olá, mundo!"
+1 PRINT "Olá, [NOME$]"
+
+[INPUT]
+Recebe um input do usuário e armazena em uma variável.
+
+Exemplo:
+0 INPUT NOME$
+
+[WAIT]
+Recebe um input qualquer do usuário e não armazena em uma variável.
+
+Exemplo:
+0 WAIT
+
+[REM]
+Comentário. Não é executado.
+
+Exemplo:
+0 REM "Isso é um comentário"
+
+[IF]
+Condicional. Executa um bloco de código se a condição for verdadeira. Caso não seja, é possível utilizar o ELSE. É possível utilizar o FIRST para verificar apenas o primeiro caractere da variável. Caso o ELSE não seja utilizado, o bloco de código será executado pela ordem dos pointers.
+
+Exemplo:
+0 IF [NOME] = "Maria" THEN 1
+1 IF [NOME] = "Maria" THEN 1 ELSE 2
+2 IF [NOME] FIRST = "M" THEN 1
+
+[GOTO]
+Vai para um pointer específico.
+
+Exemplo:
+0 GOTO 1
+
+[CLS]
+Limpa a tela.
+
+Exemplo:
+0 CLS
+
+[END]
+Finaliza o programa.
+
+Exemplo:
+0 END
---
+++ emulator.js
@@ -0,0 +1,498 @@
+let game = [];
+
+// Core Engine
+let vm = {};
+let curLn = 0;
+let waitOn = null;
+let paused = false;
+
+// Core
+function loadGame() {
+  const upload = document.createElement("input");
+  upload.type = "file";
+  upload.accept = ".questsl";
+  upload.click();
+
+  upload.onchange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const content = e.target.result;
+      game = questsl(content);
+    };
+
+    reader.readAsText(file);
+  };
+}
+
+function startGame() {
+  document.activeElement.blur();
+  cls();
+  curLn = 0;
+  vm = {};
+
+  gameLoop();
+}
+
+function gameLoop() {
+  for (let i = curLn; i < game.length; i++) {
+    const curBlock = game[i];
+
+    switch (curBlock.cmd) {
+      case "SET":
+        handleVM(curBlock.varName, curBlock.initVal);
+        break;
+      case "PRINT":
+        render(curBlock.value);
+        break;
+      case "CLS":
+        cls();
+        break;
+      case "END":
+        endGame();
+        break;
+      case "WAIT":
+        wait(i);
+        i = game.length;
+        break;
+      case "INPUT":
+        input();
+        i = game.length;
+        break;
+      case "IF":
+        curLn = handleCond(curBlock);
+        i = curLn - 1;
+        break;
+      case "GOTO":
+        curLn = goto(curBlock.value);
+        i = curLn - 1;
+      default:
+        break;
+    }
+
+    curLn++;
+  }
+}
+
+function handleVM(key, val) {
+  vm[key] = val;
+}
+
+function renderVars(str) {
+  let hasVar = str.includes("[");
+  let output = str;
+
+  if (!hasVar) {
+    return output;
+  }
+
+  while (hasVar) {
+    const start = output.indexOf("[");
+    const end = output.indexOf("]");
+
+    const varName = output.slice(start + 1, end);
+    const varVal = vm[varName];
+
+    output = output.replace(`[${varName}]`, varVal);
+    hasVar = output.includes("[");
+  }
+
+  return output;
+}
+
+function handleCond(block) {
+  const v = vm[block.vName];
+  const r = op(v, block.operator, block.opTo, block.firstOnly);
+  const out = r ? block.onTrue : block.onFalse;
+  const i = game.findIndex((b) => b.ln === out);
+
+  return i;
+}
+
+function goto(ln) {
+  const i = game.findIndex((b) => b.ln === ln);
+
+  if (i === -1) {
+    throw new Error(`Invalid GOTO command on pointer ${curLn}`);
+  }
+
+  return i;
+}
+
+function op(v, op, r, f) {
+  const c = f ? v[0] : v;
+
+  switch (op) {
+    case "=":
+      return c === r;
+    case "<":
+      return c < r;
+    case ">":
+      return c > r;
+    case "<=":
+      return c <= r;
+    case ">=":
+      return c >= r;
+    case "<>":
+      return c !== r;
+  }
+}
+
+function input() {
+  waitOn = curLn;
+
+  const emulator = document.getElementById("emulator");
+  const div = document.createElement("div");
+  div.innerHTML = ">";
+  div.className = "pinput-box";
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.id = "input";
+  input.className = "pinput";
+  input.autocomplete = "off";
+
+  div.appendChild(input);
+  emulator.appendChild(div);
+
+  input.focus();
+  input.onkeydown = (e) => {
+    if (e.key === "Enter" && input.value !== "") {
+      const varName = game[curLn - 1].varName;
+      let val = input.value;
+
+      if (!varName.includes("$")) {
+        val = Number(val);
+
+        if (isNaN(val)) {
+          return;
+        }
+      }
+
+      handleVM(varName, val);
+      input.value = "";
+      input.remove();
+      cls();
+
+      curLn = waitOn + 1;
+      waitOn = null;
+
+      gameLoop();
+    }
+  };
+}
+
+function wait(i) {
+  waitOn = i;
+  paused = true;
+
+  render("");
+  render("PRESSIONE ESPAÇO PARA CONTINUAR...");
+}
+
+function render(val) {
+  const txtToRender = renderVars(val);
+  const emulator = document.getElementById("emulator");
+
+  if (txtToRender === "") {
+    // Render break line
+    const br = document.createElement("br");
+    const br2 = document.createElement("br");
+    emulator.appendChild(br);
+    emulator.appendChild(br2);
+
+    return;
+  }
+
+  // Render text
+  const span = document.createElement("span");
+  span.innerHTML = txtToRender;
+  emulator.appendChild(span);
+}
+
+function cls() {
+  const emulator = document.getElementById("emulator");
+  emulator.innerHTML = "";
+}
+
+function endGame() {
+  render("");
+  render("FIM");
+}
+
+// Parser
+function questsl(input) {
+  try {
+    console.time("tokenize");
+    const tokens = tokenize(input);
+    console.timeEnd("tokenize");
+
+    console.time("parse");
+    const program = parseTokens(tokens, input);
+    console.timeEnd("parse");
+
+    return program;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+function tokenize(input) {
+  return input
+    .split("\n")
+    .map((line) =>
+      line
+        .trim()
+        .split(" ")
+        .filter((tkn) => tkn !== "")
+    )
+    .filter((line) => line.length > 0);
+}
+
+function parseTokens(tokens, pureStr) {
+  const acc = [];
+
+  for (let i = 0; i < tokens.length; i++) {
+    const line = tokens[i];
+    const [ln, cmd, ...rest] = line;
+
+    // validate
+    validateTokens(ln, cmd, rest, i, pureStr);
+
+    switch (cmd) {
+      case "SET":
+        const varName = rest[0];
+        const initVal = rest.slice(2).join(" ");
+        const isNumber = !varName.includes("$");
+
+        acc.push({
+          ln: Number(ln),
+          cmd,
+          varName,
+          initVal: isNumber ? Number(initVal) : initVal.replace(/"/g, ""),
+          value: rest.join(" "),
+        });
+        break;
+      case "PRINT":
+        acc.push({
+          ln: Number(ln),
+          cmd,
+          value: rest.join(" ").replace(/"/g, ""),
+        });
+        break;
+      case "INPUT":
+        const str = rest.slice(0, rest.length - 1).join(" ");
+        const inputVName = rest[rest.length - 1];
+
+        acc.push({
+          ln: Number(ln),
+          cmd,
+          varName: inputVName,
+        });
+        break;
+      case "WAIT":
+        acc.push({ ln: Number(ln), cmd });
+        break;
+      case "REM":
+        acc.push({ ln: Number(ln), cmd, value: rest.join(" ") });
+        break;
+      case "IF":
+        const firstOnly = rest.includes("FIRST");
+        const ifVName = rest[0].replace("[", "").replace("]", "");
+        const operator = firstOnly ? rest[2] : rest[1];
+        const opTo = (firstOnly ? rest[3] : rest[2]).replace(/"/g, "");
+        const onTrue = firstOnly ? Number(rest[5]) : Number(rest[4]);
+        const hasElse = rest.includes("ELSE");
+        const isString = ifVName.includes("$");
+        let onFalse = null;
+
+        if (hasElse) {
+          onFalse = firstOnly ? Number(rest[7]) : Number(rest[6]);
+        }
+
+        acc.push({
+          ln: Number(ln),
+          cmd,
+          firstOnly,
+          vName: ifVName,
+          operator,
+          opTo: isString ? opTo : Number(opTo),
+          onTrue,
+          onFalse,
+        });
+
+        break;
+      case "CLS":
+        acc.push({ ln: Number(ln), cmd });
+        break;
+      case "END":
+        acc.push({ ln: Number(ln), cmd });
+        break;
+      case "GOTO":
+        acc.push({ ln: Number(ln), cmd, value: Number(rest.join(" ")) });
+        break;
+    }
+  }
+
+  return acc;
+}
+
+function validateTokens(ln, cmd, value, i, pureStr) {
+  const commands = [
+    "SET",
+    "PRINT",
+    "INPUT",
+    "WAIT",
+    "REM",
+    "IF",
+    "CLS",
+    "END",
+    "GOTO",
+  ];
+
+  // GENERAL
+  if (!ln || isNaN(Number(ln)) || Number(ln) < 0) {
+    throw new Error(`Invalid line number on line ${i + 1}`);
+  }
+
+  if (!cmd || !commands.includes(cmd)) {
+    throw new Error(`Invalid command on pointer ${ln}`);
+  }
+
+  // SET
+  if (cmd === "SET") {
+    const varName = value[0];
+    const equal = value[1];
+    const val = value[2];
+
+    if (!equal || equal !== "=") {
+      throw new Error(`Invalid SET command on pointer ${ln}`);
+    }
+
+    if (varName.includes("$")) {
+      if (val[0] !== '"' || val[val.length - 1] !== '"') {
+        throw new Error(`Invalid string of SET command on pointer ${ln}`);
+      }
+    } else {
+      if (val[0] === '"' || val[val.length - 1] === '"') {
+        throw new Error(`Invalid number of SET command on pointer ${ln}`);
+      }
+
+      if (isNaN(Number(val))) {
+        throw new Error(`Invalid number of SET command on pointer ${ln}`);
+      }
+    }
+  }
+
+  // INPUT
+  if (cmd === "INPUT") {
+    const str = value.slice(0, value.length - 1).join(" ");
+    const varName = value[value.length - 1];
+
+    if (!varName || varName.length < 1) {
+      throw new Error(`Invalid INPUT command on pointer ${ln}`);
+    }
+
+    if (!value || value.length < 1) {
+      throw new Error(`Invalid INPUT command on pointer ${ln}`);
+    }
+
+    if (varName[varName.length - 1] !== "$") {
+      throw new Error(`Variable ${varName} not a string on pointer ${ln}`);
+    }
+
+    if (!pureStr.includes(`SET ${varName}`)) {
+      throw new Error(`Variable ${varName} not declared on pointer ${ln}`);
+    }
+  }
+
+  // IF
+  if (cmd === "IF") {
+    const operators = ["=", "<", ">", "<=", ">=", "<>"];
+    const firstOnly = value.includes("FIRST");
+    const varName = value[0].replace("[", "").replace("]", "");
+    const operator = firstOnly ? value[2] : value[1];
+    const opTo = firstOnly ? value[3] : value[2];
+    const onTrue = firstOnly ? Number(value[5]) : Number(value[4]);
+    const hasElse = value.includes("ELSE");
+    let onFalse = null;
+
+    if (hasElse) {
+      onFalse = firstOnly ? Number(value[7]) : Number(value[6]);
+    }
+
+    if (!operators.includes(operator)) {
+      throw new Error(`Invalid IF command on pointer ${ln}`);
+    }
+
+    if (!operator || !opTo) {
+      throw new Error(`Invalid IF command on pointer ${ln}`);
+    }
+
+    if (hasElse && isNaN(onFalse)) {
+      throw new Error(`Invalid IF command on pointer ${ln}`);
+    }
+
+    if (isNaN(onTrue)) {
+      throw new Error(`Invalid IF command on pointer ${ln}`);
+    }
+
+    if (!varName || varName.length < 1) {
+      throw new Error(`Invalid IF command on pointer ${ln}`);
+    }
+
+    if (!pureStr.includes(`SET ${varName}`)) {
+      throw new Error(`Variable ${varName} not declared on pointer ${ln}`);
+    }
+
+    if (varName.includes("$")) {
+      if (opTo[0] !== '"' || opTo[opTo.length - 1] !== '"') {
+        throw new Error(`Invalid type on IF command on pointer ${ln}`);
+      }
+    } else {
+      if (isNaN(Number(opTo))) {
+        throw new Error(`Invalid type on IF command on pointer ${ln}`);
+      }
+    }
+  }
+
+  // GOTO
+  if (cmd === "GOTO") {
+    const val = value.join(" ");
+
+    if (isNaN(Number(val))) {
+      throw new Error(`Invalid GOTO command on pointer ${ln}`);
+    }
+
+    if (!val || val.length < 1) {
+      throw new Error(`Invalid GOTO command on pointer ${ln}`);
+    }
+  }
+}
+
+// Keypress Events
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+
+  if (waitOn !== null && paused && key === " ") {
+    curLn = waitOn + 1;
+    waitOn = null;
+    paused = false;
+
+    cls();
+    gameLoop();
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (target.id === "emulator") {
+    const input = document.getElementById("input");
+
+    if (input) {
+      input.focus();
+    }
+  }
+});
